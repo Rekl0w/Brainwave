c7d0176e6f5788907e593e5a6caf5941216c93f6
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-04-01 17:08:37 +0300

    Add new components and fix import typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Services from "./components/Services";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +16,10 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
